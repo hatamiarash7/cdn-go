f4ada6d104920af219478400f1978bf99d01a163
Arash <136256888+arash-r1c@users.noreply.github.com>
2023-08-24 15:40:20 +0330

    Update tune.sh

--- .github/generator/tune.sh
+++ .github/generator/tune.sh
@@ -4,7 +4,7 @@ cat .github/generator/.gitignore >>.gitignore
 mv README.md docs/HOW-TO.md
 cp .github/generator/*.md .
 sed -i 's/GIT_USER_ID/arvancloud/g' go.mod test/*.go docs/*.md
-sed -i 's+GIT_REPO_ID+cdn/go-sdk+g' go.mod test/*.go docs/*.md
+sed -i 's+GIT_REPO_ID+cdn-go-sdk+g' go.mod test/*.go docs/*.md
 sed -i 's+github.com/arvancloud+git.arvancloud.ir/arvancloud+g' go.mod test/*.go docs/*.md
 sed -i 's+(docs/+(+g' docs/*.md
 sed -i 's+../README.md#+HOW-TO.md#+g' docs/*.md
